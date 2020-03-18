78d42f2e62a7a7643b94c7593d9a7273a4ff4e58
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-03-18 20:38:41 +0530

    only recipt design remaining

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { notification } from 'antd';
+import styled, { keyframes } from 'styled-components';
+import { zoomIn } from 'react-animations';
+import './styles/Receipt.css';
+
+const ZoomInAnimation = keyframes`${zoomIn}`;
+const ZoomInDiv = styled.div`
+  animation: 5s ${ZoomInAnimation};
+`;
 
 const Receipt = ({ match }) => {
   const [verifiedToken, setVerifiedToken] = useState({});
+  const [receiptAnimation, setReceiptAnimation] = useState(true);
+  const [successful, setSuccessful] = useState(false);
+  const [showReceipt, setShowReceipt] = useState(false);
 
   useEffect(() => {
     checkToken(match.params.token);
@@ -11,12 +22,27 @@ const Receipt = ({ match }) => {
 
   useEffect(() => {
     if (verifiedToken.verified) {
-      notification['success']({
-        message: 'Thank You for Donating',
-        description: `Check Your Receipt.`
-      });
+      setTimeout(() => {
+        setReceiptAnimation(false);
+        setSuccessful(true);
+      }, 1300);
+      // notification['success']({
+      //   message: 'Thank You for Donating',
+      //   description: `Check Your Receipt.`
+      // });
+    } else if (verifiedToken.verified === false) {
+      setReceiptAnimation(false);
     }
   }, [verifiedToken]);
+
+  useEffect(() => {
+    if (successful) {
+      setTimeout(() => {
+        setSuccessful(false);
+        setShowReceipt(true);
+      }, 2500);
+    }
+  }, [successful]);
 
   const checkToken = async token => {
     if (token) {
@@ -45,22 +71,85 @@ const Receipt = ({ match }) => {
       });
     }
   };
-  return (
-    <>
-      {verifiedToken.flatNo && <p>Flat Number: {verifiedToken.flatNo}</p>}
-      {/* <p>{JSON.stringify(verifiedToken)}</p> */}
-      {verifiedToken.verified && (
-        <>
-          <p>Received From: {verifiedToken.received}</p>
-          <p>Collected By: {verifiedToken.collected}</p>
-          <p>Amount: {verifiedToken.amount}</p>
-        </>
-      )}
-      {verifiedToken.timestamp && (
-        <p>Date: {Date(new Date(verifiedToken.timestamp.seconds * 1000))}</p>
-      )}
-    </>
-  );
+  if (receiptAnimation) {
+    return (
+      <div className='App'>
+        <header className='Success-header'>
+          <lottie-player
+            src='https://assets9.lottiefiles.com/packages/lf20_LfkCsp.json'
+            direction='2'
+            background='transparent'
+            speed='1'
+            style={{ width: '300px', height: '300px' }}
+            loop
+            autoplay
+          />
+        </header>
+      </div>
+    );
+  } else if (successful) {
+    return (
+      <div className='App'>
+        <header className='Success-header'>
+          <lottie-player
+            src='https://assets6.lottiefiles.com/packages/lf20_5GRn9X.json'
+            background='transparent'
+            speed='1'
+            style={{ width: '300px', height: '300px' }}
+            // loop
+            // controls
+            autoplay
+          />
+        </header>
+      </div>
+    );
+  } else if (showReceipt) {
+    return (
+      <div className='App'>
+        <header className='Success-header'>
+          <ZoomInDiv>
+            <>
+              <p>Receipt Design yet to be made.</p>
+              {verifiedToken.flatNo && (
+                <p>Flat Number: {verifiedToken.flatNo}</p>
+              )}
+              {/* <p>{JSON.stringify(verifiedToken)}</p> */}
+              {verifiedToken.verified && (
+                <>
+                  <p>Received From: {verifiedToken.received}</p>
+                  <p>Collected By: {verifiedToken.collected}</p>
+                  <p>Amount: {verifiedToken.amount}</p>
+                </>
+              )}
+              {verifiedToken.timestamp && (
+                <p>
+                  Date: {Date(new Date(verifiedToken.timestamp.seconds * 1000))}
+                </p>
+              )}
+            </>
+          </ZoomInDiv>
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <div className='App'>
+        <header className='Success-header'>
+          <ZoomInDiv>
+            <img
+              src='https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584280791/Dashboard%20Online/JMMLogoTransparent.png'
+              className='JMM-logo'
+              alt='logo'
+            />
+            <p>
+              Sorry, But due to internal server error or verification issue the
+              receipt couldn't be shown.
+            </p>
+          </ZoomInDiv>
+        </header>
+      </div>
+    );
+  }
 };
 
 export default Receipt;
