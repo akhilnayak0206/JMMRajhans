2532c943cf43907131b48641a6d0141a6053b6f8
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-03-22 20:35:29 +0530

    images added

--- src/Images.js
+++ src/Images.js
@@ -1,11 +1,11 @@
 const allImages = [
   {
     src:
-      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584885921/Covid19JMM/35.jpg'
+      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584885921/Covid19JMM/32.jpg'
   },
   {
     src:
-      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584885923/Covid19JMM/32.jpg'
+      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584885923/Covid19JMM/34.jpg'
   },
   {
     src:
@@ -13,15 +13,7 @@ const allImages = [
   },
   {
     src:
-      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584807021/Covid19JMM/22.jpg'
-  },
-  {
-    src:
-      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584885918/Covid19JMM/34.jpg'
-  },
-  {
-    src:
-      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584807021/Covid19JMM/21.jpg'
+      'https://res.cloudinary.com/dx0wpoeyu/image/upload/v1584885918/Covid19JMM/35.jpg'
   }
 ];
 
